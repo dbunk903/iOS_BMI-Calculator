228d22dd040163c16a12133b80e8b9ea918045a3
dbunk903 <dbunk903@gmail.com>
2022-04-08 10:47:39 +0900

    My way of doing 2 decimal conversion

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func heightSliderChanged(_ sender: UISlider) {
+        sender.value = round(sender.value*100)/100
         print(sender.value)
+
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
