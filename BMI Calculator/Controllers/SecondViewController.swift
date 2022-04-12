1a5c8dbd2301641ee97aa5c5af67f5bcfd961f7b
dbunk903 <dbunk903@gmail.com>
2022-04-12 15:09:51 +0900

    done implementing ViewController

--- BMI Calculator/Controllers/SecondViewController.swift
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -8,6 +8,14 @@ import UIKit
 class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        //either UIColor.red or just .red because Xcode knows following code must be UIColor
+        view.backgroundColor = .red
+        let label = UILabel()
+        label.text = "Hello"
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        // put label onto screen
+        // UILabel is inheriting UIView
+        view.addSubview(label)
     }
 }
 
