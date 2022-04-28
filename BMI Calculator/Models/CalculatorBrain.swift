7df6a7d44c18ced8b2cd521de1413e3d9c280169
dbunk903 <dbunk903@gmail.com>
2022-04-28 15:40:27 +0900

    Add Color

--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -5,7 +5,8 @@
 //  Created by Jero Lee on 2022/04/18.
 //
 
-import Foundation
+import UIKit
+
 
 struct CalculatorBrain {
     
@@ -19,12 +20,13 @@ struct CalculatorBrain {
     
     mutating func calculateBMI (height:Float, weight:Float){
         let bmiValue = weight / pow(height, 2)
+        let color = (underweight: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), healthy: #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1), overweight: #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1))
         if bmiValue < 18.5{
-            print("underweight")
+            bmi = BMI(value: bmiValue, advice: "Eat more pies", color: color.healthy)
         }else if bmiValue < 24.9 {
-            print("normal weight")
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: color.healthy)
         }else {
-            print("overweight")
+            bmi = BMI(value: bmiValue, advice: "You gotta be Careful man", color: color.overweight)
         }
     }
     
