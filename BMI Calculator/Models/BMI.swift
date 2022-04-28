dbad7d0d02cdb4c7ad37640cf380a7d419df863f
dbunk903 <dbunk903@gmail.com>
2022-04-28 15:21:15 +0900

    BMI Modeling

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Jero Lee on 2022/04/28.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice : String
+    let color : UIColor
+}
